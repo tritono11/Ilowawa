169f8f4c4286415bb56319496e8c897f92fdefb8
ciloozz <ciloo@hotmail.it>
2018-01-25 16:39:47 +0100

    Primo branch activation email rev3

--- app/User.php
+++ app/User.php
@@ -6,6 +6,7 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Laravel\Passport\HasApiTokens;
 
+
 class User extends Authenticatable
 {
     use Notifiable;
